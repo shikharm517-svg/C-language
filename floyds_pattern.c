5227bbb6c100893627b1b2d9c492906211ae2dc3
Shikhar Mishra <shikharm517@gmail.com>
2026-08-25 13:44:43 +0530

    update

---
+++ floyds_pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, num = 1;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= i; j++) {
+            printf("%3d ", num);
+            num++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
